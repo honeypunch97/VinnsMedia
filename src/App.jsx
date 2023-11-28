a98e2b1002355e45fbd943ba5904153ee1164677
honeypunch97 <bkws04211@naver.com>
2023-11-28 23:05:40 +0900

    Fix : change BrowerRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, Link, createHashRouter } from "react-router-dom";
 import SurveyList from "./routers/SurveyList";
 import SurveyRegister from "./routers/SurveyRegister";
 import SurveyDetail from "./routers/SurveyDetail";
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/", element: <SurveyList /> },
   { path: "/register", element: <SurveyRegister /> },
   { path: "/survey/:surveyId", element: <SurveyDetail /> },
